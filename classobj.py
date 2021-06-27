f0c863617dd0e3cf3c9c8b6448c32fa228dab0de
Suresh789k <85558478+Suresh789k@users.noreply.github.com>
2021-06-27 22:56:15 +0530

    class created

---
+++ classobj.py
@@ -0,0 +1,11 @@
+class A
+   def __init__(self,name,age)
+       self.name=name;
+       self.age=age;
+   def details();
+       print("your name is" self.name)
+       print("your age is", self.age)
+      
+ obj1=A()
+obj1.details()
+      
